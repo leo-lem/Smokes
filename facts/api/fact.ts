9db53a8fdea814c53cfef03ef78fc902dc4830d5
Leopold Lemmermann <leopold@lemmermann.xyz>
2025-07-27 14:07:48 +0200

    Add lightweight logging and add headers.

--- facts/api/fact.ts
+++ facts/api/fact.ts
@@ -6,10 +6,15 @@ type Fact = {
 }
 
 export async function GET(): Promise<Response> {
-  const filePath = new URL('./facts.json', import.meta.url).pathname
-  const data = await readFile(filePath, 'utf-8')
+  const data = await readFile(new URL('./facts.json', import.meta.url).pathname, 'utf-8')
   const facts: Fact[] = JSON.parse(data)
-
   const random = facts[Math.floor(Math.random() * facts.length)]
-  return Response.json(random)
+
+  console.log('Random fact requested: ', random)
+
+  const headers = new Headers()
+  headers.set('Access-Control-Allow-Origin', '*')
+  headers.set('Content-Type', 'application/json')
+
+  return new Response(JSON.stringify(random), { headers })
 }
