da71d7f6a5ce70f63f6b554ddf2813e329f14cff
Leopold Lemmermann <leopold@lemmermann.xyz>
2025-07-27 11:36:31 +0200

    add vercel.

---
+++ api/fact/index.ts
@@ -0,0 +1,13 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+import facts from './_facts.json'
+
+type Fact = {
+  fact: string
+  source: string
+}
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const random: Fact = facts[Math.floor(Math.random() * facts.length)]
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.status(200).json(random)
+}
